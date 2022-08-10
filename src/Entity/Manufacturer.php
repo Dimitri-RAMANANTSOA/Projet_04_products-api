85ca4f434df2efd201190a628e3d1ba56d8e3801
Dimitri RAMANANTSOA <dimitri.rama.pro@gmail.com>
2022-08-10 05:49:00 +0200

    create Product Entity + relation to Manufacturer + Validation

--- src/Entity/Manufacturer.php
+++ src/Entity/Manufacturer.php
@@ -2,10 +2,18 @@
 
 namespace App\Entity;
 
+use App\Entity\Product;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\ManufacturerRepository;
+use Doctrine\Common\Collections\Collection;
 use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Validator\Constraints\Date;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\Country;
+use Symfony\Component\Validator\Constraints\NotNull;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 #[ORM\Entity(repositoryClass: ManufacturerRepository::class)]
 /** A Manufacturer */
@@ -19,24 +27,47 @@ class Manufacturer
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[
+        Length(min: 5),
+        NotBlank()
+    ]
     /** Manufacturer Name */
     private ?string $name = null;
 
     #[ORM\Column(length: 255)]
+    #[
+        Length(min: 10),
+        NotBlank()
+    ]
     /** Manufacturer Description */
     private ?string $description = null;
 
     #[ORM\Column(length: 255)]
+    #[
+        Length(min: 5),
+        NotBlank()
+    ]
     /** Manufacturer CountryCode */
+    #[
+        Country(),
+        NotBlank()
+    ]
     private ?string $countryCode = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[
+        NotNull()
+    ]
     /** Manufacturer Listed Date */
     private ?\DateTimeInterface $listedAt = null;
+
+    #[ORM\OneToMany(mappedBy: 'manufacturer', targetEntity: Product::class, cascade: ['persist', 'remove'])]
+    private Collection $products;
 
     public function __construct()
     {
         $this->listedAt = new \DateTime();
+        $this->products = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -88,6 +119,36 @@ class Manufacturer
     public function setListedAt(\DateTimeInterface $listedAt): self
     {
         $this->listedAt = $listedAt;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Product>
+     */
+    public function getProducts(): Collection
+    {
+        return $this->products;
+    }
+
+    public function addProduct(Product $product): self
+    {
+        if (!$this->products->contains($product)) {
+            $this->products->add($product);
+            $product->setManufacturer($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProduct(Product $product): self
+    {
+        if ($this->products->removeElement($product)) {
+            // set the owning side to null (unless already changed)
+            if ($product->getManufacturer() === $this) {
+                $product->setManufacturer(null);
+            }
+        }
 
         return $this;
     }
---
+++ src/Entity/Product.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\ProductRepository;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Validator\Constraints\Date;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+/** A Product */
+#[ApiResource]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    /** Product ID */
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    #[
+        Length(min: 5, max: 255),
+        NotBlank()
+    ]
+    /** The MPN (Manufacturer Part Number) of the product */
+    private ?string $mpn = null;
+
+    #[ORM\Column(length: 255)]
+    #[
+        Length(min: 5, max: 255),
+        NotBlank()
+    ]
+    /** Product Name */
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    #[
+        Length(min: 10, max: 255),
+        NotBlank()
+    ]
+    /** Product description */
+    private ?string $description = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[
+        NotBlank()
+    ]
+    /** The date of issue of the product */
+    private ?\DateTimeInterface $issueAt = null;
+
+    #[ORM\ManyToOne(inversedBy: 'products')]
+    #[
+        NotBlank()
+    ]
+    /** The Manufacturer of the product */
+    private ?Manufacturer $manufacturer = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMpn(): ?string
+    {
+        return $this->mpn;
+    }
+
+    public function setMpn(?string $mpn): self
+    {
+        $this->mpn = $mpn;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getIssueAt(): ?\DateTimeInterface
+    {
+        return $this->issueAt;
+    }
+
+    public function setIssueAt(\DateTimeInterface $issueAt): self
+    {
+        $this->issueAt = $issueAt;
+
+        return $this;
+    }
+
+    public function getManufacturer(): ?Manufacturer
+    {
+        return $this->manufacturer;
+    }
+
+    public function setManufacturer(?Manufacturer $manufacturer): self
+    {
+        $this->manufacturer = $manufacturer;
+
+        return $this;
+    }
+}
